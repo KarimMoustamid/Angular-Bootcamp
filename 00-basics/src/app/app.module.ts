ce35e558116904213df96ee868d5fefa1d643194
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-07 10:43:33 -0500

    My First Angular Component

--- 00-basics/src/app/app.module.ts
+++ 00-basics/src/app/app.module.ts
@@ -3,8 +3,14 @@ import { NgModule } from '@angular/core';
 // !  Browser Specific Platform :
 import { BrowserModule } from '@angular/platform-browser';
 
+// . Importing App.Component.ts Class :
+import { AppComponent } from './app.component';
+
 @NgModule({
-  //- Registering the BrowserModule :
+  //. Registering the BrowserModule :
   imports: [BrowserModule],
+  declarations: [AppComponent],
+  //. Defining the Root Component
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
