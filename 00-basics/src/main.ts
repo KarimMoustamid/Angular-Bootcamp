e8eece2b3ef5de970d7765013bca7d07e19ccfd1
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-07 09:50:25 -0500

    Adding Production Mode

--- 00-basics/src/main.ts
+++ 00-basics/src/main.ts
@@ -1,11 +1,19 @@
 //! Responsibility of the main.ts , is to load Angular and our Aplication code .
 // # Angular Provide two types of Compilations For TS to JS : JIT and AOT .
 
+//- Production Mode : Controlling Change detection :
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
 // - Loading Angular Platform For JIT :
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 //- Loading Our Application :
 import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
