5a2ae259839d389b35004a359bb71a8cb1bd6c6f
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-07 11:47:29 -0500

    Interpolation in Angular

--- 00-basics/src/app/app.component.ts
+++ 00-basics/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   // styles: [''],
 })
-export class AppComponent {}
+export class AppComponent {
+  name = 'Karim';
+
+  getName() {
+    return this.name;
+  }
+}
