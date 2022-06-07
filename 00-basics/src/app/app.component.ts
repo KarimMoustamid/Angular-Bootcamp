2526625b64d2fc1225cf0630d82594201b2b880f
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-07 11:05:51 -0500

    External Template and Styles

--- 00-basics/src/app/app.component.ts
+++ 00-basics/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello world</h1>`,
+  // template: `<h1>Hello world</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // styles: [''],
 })
 export class AppComponent {}
