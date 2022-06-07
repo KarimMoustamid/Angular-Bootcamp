2c6f48f70ff8d114b96cd3e9d75768b0ffa5959e
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-07 11:54:05 -0500

    Property Binding

--- 00-basics/src/app/app.component.ts
+++ 00-basics/src/app/app.component.ts
@@ -10,6 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Karim';
+  imageURL =
+    'https://images.unsplash.com/photo-1615754925954-435b08409d16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
 
   getName() {
     return this.name;
