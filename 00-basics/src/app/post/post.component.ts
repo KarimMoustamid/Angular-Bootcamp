19d85f8ee6cdd79ee48c7f511b54ab763650ea3b
KarimMoustamid <105139320+KarimMoustamid@users.noreply.github.com>
2022-06-09 07:00:09 -0500

    LifeCycle Hooks

--- 00-basics/src/app/post/post.component.ts
+++ 00-basics/src/app/post/post.component.ts
@@ -1,11 +1,73 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('img') postImage = '';
   @Output() imgSelected = new EventEmitter<string>();
+
+  constructor() {
+    console.log('Constructor called', this.postImage);
+  }
+
+  ngOnInit() {
+    console.log('Initializing : ngonInit called', this.postImage);
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges : Changes');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck : Change Detection Checked');
+  }
+
+  // . Projected Content :
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  // . components template , runs every time ngDoCheck runs:
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('noOnDestroy called');
+  }
 }
